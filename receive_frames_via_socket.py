554f33ad3100fc167ce6bdfeb6f94f50c31a9cdb
VaclavNezerka <vaclav.nezerka@gmail.com>
2023-07-26 09:39:37 +0200

    Cosmetic changes, Venca's IP address defined.

--- receive_frames_via_socket.py
+++ receive_frames_via_socket.py
@@ -3,7 +3,8 @@ import cv2
 import pickle
 import struct
 
-HOST = "169.254.73.204"
+# HOST = "169.254.73.204"
+HOST = "192.168.0.101"
 PORT = 1024
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -17,17 +18,17 @@ conn, addr = s.accept()
 
 data = b""
 payload_size = struct.calcsize(">L")
-print("payload_size: {}".format(payload_size))
+print("Payload_size: {}".format(payload_size))
 while True:
     while len(data) < payload_size:
-        print("Recv: {}".format(len(data)))
+        # print("Recv: {}".format(len(data)))
         data += conn.recv(4096)
 
-    print("Done Recv: {}".format(len(data)))
+    # print("Done Recv: {}".format(len(data)))
     packed_msg_size = data[:payload_size]
     data = data[payload_size:]
     msg_size = struct.unpack(">L", packed_msg_size)[0]
-    print("Message size: {}".format(msg_size))
+    # print("Message size: {}".format(msg_size))
     while len(data) < msg_size:
         data += conn.recv(4096)
     frame_data = data[:msg_size]
